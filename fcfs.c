e4a1fa509005dcc48ed6b5d6296c4cb1085e0578
LYerger <lorraine.yerger@gmail.com>
2018-02-13 11:24:39 -0500

    Completed arrival and running algorithms

--- fcfs.c
+++ fcfs.c
@@ -8,7 +8,74 @@ void fcfs(process **processesArray, schedparams *parameters)
 	if(fp==NULL)
 		printf("Error: file not found.\n");
 	
-	fputs("hello!\n", fp);
+	fprintf(fp, "%d Processes\n", parameters->processCount);
+	fprintf(fp, "Using First Come First Served\n\n");
+	
+	int time;
+	int indexOfNextArriving=0;
+	int allProcessesArrived = 0;
+	int indexOfProcessToRun = 0; // bc processessArray is sorted by arrival time
+	int processStartTime = 0;
+	
+	for(time=0; time<parameters->runTime; time++)
+	{
+		// --- CHECK FOR ARRIVING PROCESSES ---
+		// Are there processes left to arrive?
+		if(!allProcessesArrived)
+		{
+			// Did any process just arrive?
+			if(processesArray[indexOfNextArriving]->arrival_time == time)
+			{
+				// Write to Output
+				fprintf(fp, "Time %d: %s arrived\n", time, processesArray[indexOfNextArriving]->process_name);
+				processesArray[indexOfNextArriving]->process_state = READY;
+				printf("here 1\n");
+				indexOfNextArriving++;
+
+				
+				// Was this the last process to arrive? 
+				if(indexOfNextArriving==parameters->processCount)
+					allProcessesArrived = 1;
+			}
+		}
+		
+		// --- CHECK WHICH PROCESS TO RUN ---
+		// Are we currently running a process?
+		if(processesArray[indexOfProcessToRun]->process_state == RUNNING)
+		{
+			// Did the process just finish?
+			if( (time-processStartTime) == (processesArray[indexOfProcessToRun]->burst_length) )
+			{
+				// Print finished message, set state to READY, increase indexOfProcessToRun
+				fprintf(fp, "Time %d: %s finished\n", time, processesArray[indexOfProcessToRun]->process_name);
+				processesArray[indexOfProcessToRun]->process_state = READY;
+				indexOfProcessToRun++;
+			}
+			
+			// Or, is process still running? If so, do nothing.
+			else
+				;
+				// fprintf(fp, "Time %d - process %s still running, run time=%d\n", time, 
+// 								processesArray[indexOfProcessToRun]->process_name, 
+// 								time-processStartTime);
+		}
+			;
+		// Else, if we need to start a new process, has p[indexOfProcessToRun] arrived?
+		if(processesArray[indexOfProcessToRun]->process_state == READY)
+		{
+			// Start running this process
+			processesArray[indexOfProcessToRun]->process_state = RUNNING;
+			processStartTime=time;
+			fprintf(fp, "Time %d: %s selected (burst %d)\n", time, processesArray[indexOfProcessToRun]->process_name,
+							processesArray[indexOfProcessToRun]->burst_length);
+		}
+		
+	}
+	// Check if last process running finished at end of runTime
+	if( (time-processStartTime) == (processesArray[indexOfProcessToRun]->burst_length) )
+		fprintf(fp, "Time %d: %s finished\n", time, processesArray[indexOfProcessToRun]->process_name);
+	
+	fprintf(fp, "Finished at time %d\n\n", time);
 	
 	return;
 }
