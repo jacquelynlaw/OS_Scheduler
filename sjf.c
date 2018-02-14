7700d70f2b344d44661c325d9372a44a108465a2
Brandon Cuevas <cuevasb711@gmail.com>
2018-02-14 02:45:00 -0500

    implement sjf.c
    - no comprehensive debugging yet, but seems to work at least with sample sjf process (set 3)
    - prints final process stats sorted by arrival time instead of original order; hopefully this isn't an issue

--- sjf.c
+++ sjf.c
@@ -1,7 +1,155 @@
 #include "header.h"
 
+#define INT_MAX 2147483647
+#define TERMINATED 2
+#define PROCESS_IDLE -1
+#define OUTPUT_FILE "processes.out"
+
+// Functional Prototypes
+int selectProcess(process **processesArray, int processCount);
+void incrementTurnaroundTimes(int currentProcess, int *waitTimes, int processCount, process **processesArray);
+void incrementWaitTimes(int currentProcess, int *waitTimes, int processCount, process **processesArray);
+
+// Shortest Job First Scheduling
+// Assumes processesArray is sorted by arrival time
+// ### Prints process statistics in sorted order, NOT original order ###
 void sjf(process **processesArray, schedparams *parameters)
 {
-	printf("Running SJF from sjf.c...\n");
-	return;
+	int processCount = parameters->processCount;
+	int runTime = parameters->runTime;
+	int i, t, nextArrivingProcess = 0, currentProcess = PROCESS_IDLE;
+	int *waitTimes, *turnaroundTimes;
+
+	FILE *ofp = fopen(OUTPUT_FILE, "w");
+
+	// Track wait & turnaround times for each process
+	waitTimes = calloc(processCount, sizeof(int));
+	turnaroundTimes = calloc(processCount, sizeof(int));
+
+	// Print output file headings
+	fprintf(ofp, "%d processes\n", processCount);
+	fprintf(ofp, "Using Shortest Job First\n\n");
+
+	// Increment through full run time
+	for (t = 0; t < runTime; t++)
+	{
+		incrementTurnaroundTimes(currentProcess, turnaroundTimes, processCount, processesArray);
+		incrementWaitTimes(currentProcess, waitTimes, processCount, processesArray);
+
+		// If a process is running, decrement its burst_length and check for termination
+		if (currentProcess != PROCESS_IDLE)
+		{
+			processesArray[currentProcess]->burst_length--;
+
+			if (processesArray[currentProcess]->burst_length <= 0)
+			{
+				fprintf(ofp, "Time %d: %s finished\n", t, processesArray[currentProcess]->process_name);
+
+				processesArray[currentProcess]->process_state = TERMINATED;
+				currentProcess = PROCESS_IDLE;
+			}
+		}
+
+		// Check for process arrival
+		if (processesArray[nextArrivingProcess]->arrival_time == t)
+		{
+			// Set newly arrived process to READY
+			processesArray[nextArrivingProcess]->process_state = READY;
+
+			fprintf(ofp, "Time %d: %s arrived\n", t, processesArray[nextArrivingProcess]->process_name);
+
+			// Assumes next process index is also next arrival
+			if (nextArrivingProcess < processCount - 1)
+			{
+				nextArrivingProcess++;
+			}
+
+			// Select next process to run
+			currentProcess = selectProcess(processesArray, processCount);
+			if (currentProcess != PROCESS_IDLE)
+			{
+				fprintf(ofp, "Time %d: %s selected (burst %d)\n", t, processesArray[currentProcess]->process_name, processesArray[currentProcess]->burst_length);
+			}
+		}
+
+		// If no process is running, select a new process or declare idle
+		if (currentProcess == PROCESS_IDLE)
+		{
+			currentProcess = selectProcess(processesArray, processCount);
+
+			if (currentProcess != PROCESS_IDLE)
+			{
+				fprintf(ofp, "Time %d: %s selected (burst %d)\n", t, processesArray[currentProcess]->process_name, processesArray[currentProcess]->burst_length);
+			}
+			else
+			{
+				fprintf(ofp, "Time %d: Idle\n", t);
+			}
+		}
+
+	}
+
+	// Print final results
+	fprintf(ofp, "Finished at time %d\n\n", t);
+
+	for (i = 0; i < processCount; i++)
+	{
+		fprintf(ofp, "%s wait %d turnaround %d", processesArray[i]->process_name, waitTimes[i], turnaroundTimes[i]);
+		if (i < (processCount - 1))
+		{
+			fprintf(ofp, "\n");
+		}
+	}
+
+	free(waitTimes);
+	free(turnaroundTimes);
+
+	fclose(ofp);
+}
+
+// Return index of process with smallest burst_length that is ready to run
+int selectProcess(process **processesArray, int processCount)
+{
+	int i, selectedProcess = PROCESS_IDLE, min = INT_MAX;
+	for (i = 0; i < processCount; i++)
+	{
+		if (processesArray[i]->process_state == READY)
+		{
+			if (processesArray[i]->burst_length < min)
+			{
+				selectedProcess = i;
+				min = processesArray[i]->burst_length;
+			}
+		}
+	}
+	return selectedProcess;
+}
+
+// Increment turnaround times for each process
+void incrementTurnaroundTimes(int currentProcess, int *turnaroundTimes, int processCount, process **processesArray)
+{
+	int i;
+	for (i = 0; i < processCount; i++)
+	{
+		if (processesArray[i]->process_state == READY)
+		{
+			turnaroundTimes[i]++;
+		}
+	}
+}
+
+// Increment wait times for each process (except current process)
+void incrementWaitTimes(int currentProcess, int *waitTimes, int processCount, process **processesArray)
+{
+	int i;
+	for (i = 0; i < processCount; i++)
+	{
+		if (processesArray[i]->process_state == READY)
+		{
+			if (i != currentProcess)
+			{
+				waitTimes[i]++;
+			}
+		}
+	}
 }
